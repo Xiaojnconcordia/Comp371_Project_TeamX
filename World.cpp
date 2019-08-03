7036d7efa8126e9aa96d8f750accf7396d04d27c
G-J-Y <48920292+G-J-Y@users.noreply.github.com>
2019-08-03 14:10:43 -0400

    Add files via upload

---
+++ World.cpp
@@ -0,0 +1,338 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2019 Concordia University. All rights reserved.
+//
+
+#include "World.h"
+#include "Renderer.h"
+#include "ParsingHelper.h"
+
+#include "StaticCamera.h"
+#include "FirstPersonCamera.h"
+
+#include "CubeModel.h"
+#include "SphereModel.h"
+#include "Animation.h"
+#include <GLFW/glfw3.h>
+#include "EventManager.h"
+
+#include "BSpline.h"
+#include "BSplineCamera.h"
+
+using namespace std;
+using namespace glm;
+
+World* World::instance;
+
+
+World::World()
+{
+    instance = this;
+
+	
+	ambientSphere = 0.01;
+	diffuseSphere = 0.8;
+	specularSphere = 0.3;
+	exponentSphere = 50.0;
+
+
+	lightColor = vec3(1.0f, 1.0f, 1.0f);
+	lightPosition = vec4(0.0f, 2.0f, 0.0f, 1.0f);
+	kc = 0.0f;
+	kl = 0.0f;
+	kq = 0.2f;
+
+
+
+	// Setup Camera
+	mCamera.push_back(new FirstPersonCamera(vec3(3.0f, 5.0f, 20.0f)));
+	mCamera.push_back(new StaticCamera(vec3(3.0f, 30.0f, 5.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f)));
+	mCamera.push_back(new StaticCamera(vec3(0.5f,  0.5f, 5.0f), vec3(0.0f, 0.5f, 0.0f), vec3(0.0f, 1.0f, 0.0f)));
+	mCurrentCamera = 0;
+}
+
+World::~World()
+{
+	// Models
+	for (vector<Model*>::iterator it = mModel.begin(); it < mModel.end(); ++it)
+	{
+		delete *it;
+	}
+
+	mModel.clear();
+
+	for (vector<Animation*>::iterator it = mAnimation.begin(); it < mAnimation.end(); ++it)
+	{
+		delete *it;
+	}
+
+	mAnimation.clear();
+
+	for (vector<AnimationKey*>::iterator it = mAnimationKey.begin(); it < mAnimationKey.end(); ++it)
+	{
+		delete *it;
+	}
+
+	mAnimationKey.clear();
+
+	// Camera
+	for (vector<Camera*>::iterator it = mCamera.begin(); it < mCamera.end(); ++it)
+	{
+		delete *it;
+	}
+	mCamera.clear();
+}
+
+World* World::GetInstance()
+{
+    return instance;
+}
+
+void World::Update(float dt)
+{
+	// User Inputs
+	// 0 1 2 to change the Camera
+	if (glfwGetKey(EventManager::GetWindow(), GLFW_KEY_1 ) == GLFW_PRESS)
+	{
+		mCurrentCamera = 0;
+	}
+	else if (glfwGetKey(EventManager::GetWindow(), GLFW_KEY_2 ) == GLFW_PRESS)
+	{
+		if (mCamera.size() > 1)
+		{
+			mCurrentCamera = 1;
+		}
+	}
+	else if (glfwGetKey(EventManager::GetWindow(), GLFW_KEY_3 ) == GLFW_PRESS)
+	{
+		if (mCamera.size() > 2)
+		{
+			mCurrentCamera = 2;
+		}
+	}
+
+	// Spacebar to change the shader
+	if (glfwGetKey(EventManager::GetWindow(), GLFW_KEY_0 ) == GLFW_PRESS)
+	{
+		Renderer::SetShader(SHADER_SOLID_COLOR);
+	}
+	else if (glfwGetKey(EventManager::GetWindow(), GLFW_KEY_9 ) == GLFW_PRESS)
+	{
+		Renderer::SetShader(SHADER_BLUE);
+	}
+
+    // Update animation and keys
+    for (vector<Animation*>::iterator it = mAnimation.begin(); it < mAnimation.end(); ++it)
+    {
+        (*it)->Update(dt);
+    }
+    
+    for (vector<AnimationKey*>::iterator it = mAnimationKey.begin(); it < mAnimationKey.end(); ++it)
+    {
+        (*it)->Update(dt);
+    }
+
+
+	// Update current Camera
+	mCamera[mCurrentCamera]->Update(dt);
+
+	// Update models
+	for (vector<Model*>::iterator it = mModel.begin(); it < mModel.end(); ++it)
+	{
+		(*it)->Update(dt);
+	
+	}
+}
+
+void World::Draw()
+{
+	Renderer::BeginFrame();
+	
+	// Set shader to use
+	glUseProgram(Renderer::GetShaderProgramID());
+
+	// This looks for the MVP Uniform variable in the Vertex Program
+	GLuint VPMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "ViewProjectionTransform");
+
+
+	//project
+	Renderer::SetShader(SHADER_PHONG);
+	GLuint ViewMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "ViewTransform");
+	GLuint ProjectionMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "ProjectonTransform");
+
+	GLuint LightPositionID = glGetUniformLocation(Renderer::GetShaderProgramID(), "WorldLightPosition");
+	GLuint LightColorID = glGetUniformLocation(Renderer::GetShaderProgramID(), "lightColor");
+	GLuint LightAttenuationID = glGetUniformLocation(Renderer::GetShaderProgramID(), "lightAttenuation");
+
+	GLuint MaterialID = glGetUniformLocation(Renderer::GetShaderProgramID(), "materialCoefficients");
+
+
+	// Send the view projection constants to the shader
+	mat4 VP = mCamera[mCurrentCamera]->GetViewProjectionMatrix();
+	glUniformMatrix4fv(VPMatrixLocation, 1, GL_FALSE, &VP[0][0]);
+
+	// Draw models
+	for (vector<Model*>::iterator it = mModel.begin(); it < mModel.end(); ++it)
+	{
+		//project
+		glUniform4f(MaterialID, ambientSphere, diffuseSphere, specularSphere, exponentSphere);
+		glUniform4f(LightPositionID, lightPosition.x, lightPosition.y, lightPosition.z, lightPosition.w);
+		glUniform3f(LightColorID, lightColor.r, lightColor.g, lightColor.b);
+		glUniform3f(LightAttenuationID, kc, kl, kq);
+
+		glUniformMatrix4fv(ViewMatrixLocation, 1, GL_FALSE, &(mCamera[mCurrentCamera]->GetViewMatrix())[0][0]);
+		glUniformMatrix4fv(ProjectionMatrixLocation, 1, GL_FALSE, &(mCamera[mCurrentCamera]->GetProjectionMatrix())[0][0]);
+
+		(*it)->Draw();
+	}
+
+	// Draw Path Lines
+	
+	// Set Shader for path lines
+	unsigned int prevShader = Renderer::GetCurrentShader();
+	Renderer::SetShader(SHADER_PATH_LINES);
+	glUseProgram(Renderer::GetShaderProgramID());
+    
+
+	// Send the view projection constants to the shader
+	VPMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "ViewProjectionTransform");
+	glUniformMatrix4fv(VPMatrixLocation, 1, GL_FALSE, &VP[0][0]);
+
+    for (vector<BSpline*>::iterator it = mSpline.begin(); it < mSpline.end(); ++it)
+    {
+        (*it)->Draw();
+    }
+    
+	for (vector<Animation*>::iterator it = mAnimation.begin(); it < mAnimation.end(); ++it)
+	{
+		(*it)->Draw();
+	}
+
+	for (vector<AnimationKey*>::iterator it = mAnimationKey.begin(); it < mAnimationKey.end(); ++it)
+	{
+		mat4 VP = mCamera[mCurrentCamera]->GetViewProjectionMatrix();
+		glUniformMatrix4fv(VPMatrixLocation, 1, GL_FALSE, &VP[0][0]);
+
+		(*it)->Draw();
+	}
+    
+    Renderer::CheckForErrors();
+    
+	// Restore previous shader
+	Renderer::SetShader((ShaderType) prevShader);
+
+	Renderer::EndFrame();
+}
+
+void World::LoadScene(const char * scene_path)
+{
+	// Using case-insensitive strings and streams for easier parsing
+	ci_ifstream input;
+	input.open(scene_path, ios::in);
+
+	// Invalid file
+	if(input.fail() )
+	{	 
+		fprintf(stderr, "Error loading file: %s\n", scene_path);
+		getchar();
+		exit(-1);
+	}
+
+	ci_string item;
+	while( std::getline( input, item, '[' ) )   
+	{
+        ci_istringstream iss( item );
+
+		ci_string result;
+		if( std::getline( iss, result, ']') )
+		{
+			if( result == "cube" )
+			{
+				// Box attributes
+				CubeModel* cube = new CubeModel();
+				cube->Load(iss);
+				mModel.push_back(cube);
+			}
+            else if( result == "sphere" )
+            {
+                SphereModel* sphere = new SphereModel();
+                sphere->Load(iss);
+                mModel.push_back(sphere);
+            }
+			else if ( result == "animationkey" )
+			{
+				AnimationKey* key = new AnimationKey();
+				key->Load(iss);
+				mAnimationKey.push_back(key);
+			}
+			else if (result == "animation")
+			{
+				Animation* anim = new Animation();
+				anim->Load(iss);
+				mAnimation.push_back(anim);
+			}
+            else if (result == "spline")
+            {
+                BSpline* spline = new BSpline();
+                spline->Load(iss);
+                spline->CreateVertexBuffer();
+                
+                // FIXME: This is hardcoded: replace last camera with spline camera
+                mSpline.push_back(spline);
+                mCamera.pop_back();
+                mCamera.push_back(new BSplineCamera(spline, 10.0f));
+            }
+			else if ( result.empty() == false && result[0] == '#')
+			{
+				// this is a comment line
+			}
+			else
+			{
+				fprintf(stderr, "Error loading scene file... !");
+				getchar();
+				exit(-1);
+			}
+	    }
+	}
+	input.close();
+
+	// Set Animation vertex buffers
+	for (vector<Animation*>::iterator it = mAnimation.begin(); it < mAnimation.end(); ++it)
+	{
+		// Draw model
+		(*it)->CreateVertexBuffer();
+	}
+}
+
+Animation* World::FindAnimation(ci_string animName)
+{
+    for(std::vector<Animation*>::iterator it = mAnimation.begin(); it < mAnimation.end(); ++it)
+    {
+        if((*it)->GetName() == animName)
+        {
+            return *it;
+        }
+    }
+    return nullptr;
+}
+
+AnimationKey* World::FindAnimationKey(ci_string keyName)
+{
+    for(std::vector<AnimationKey*>::iterator it = mAnimationKey.begin(); it < mAnimationKey.end(); ++it)
+    {
+        if((*it)->GetName() == keyName)
+        {
+            return *it;
+        }
+    }
+    return nullptr;
+}
+
+const Camera* World::GetCurrentCamera() const
+{
+     return mCamera[mCurrentCamera];
+}
---
+++ World.h
@@ -0,0 +1,65 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2019 Concordia University. All rights reserved.
+//
+
+#pragma once
+
+
+#include "ParsingHelper.h"
+#include <vector>
+#include <glm/glm.hpp>
+
+class Camera;
+class Model;
+class Animation;
+class AnimationKey;
+class BSpline;
+using namespace glm;
+
+class World
+{
+public:
+	World();
+	~World();
+	
+    static World* GetInstance();
+
+	void Update(float dt);
+	void Draw();
+
+	void LoadScene(const char * scene_path);
+    Animation* FindAnimation(ci_string animName);
+    AnimationKey* FindAnimationKey(ci_string keyName);
+
+    const Camera* GetCurrentCamera() const;
+    
+private:
+    static World* instance;
+    
+	std::vector<Model*> mModel;
+    std::vector<Animation*> mAnimation;
+    std::vector<AnimationKey*> mAnimationKey;
+	std::vector<Camera*> mCamera;
+    std::vector<BSpline*> mSpline;
+	unsigned int mCurrentCamera;
+
+
+	//Adding light properties from assignment 2
+
+	float ambientSphere;
+	float diffuseSphere;
+	float specularSphere;
+	float exponentSphere;
+
+	float kc;
+	float kl;
+	float kq;
+
+	glm::vec3 lightColor;
+	glm::vec4 lightPosition;
+};
